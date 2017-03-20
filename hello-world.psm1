3e7134dd86858ec715664eccd940236cc47bce19
MiZeAT <michael.zenzmaier@gmail.com>
2017-03-20 21:51:24 +0100

    Add files via upload

---
+++ hello-world.psm1
@@ -0,0 +1,51 @@
+
+<#PSScriptInfo
+
+.VERSION 2.0
+
+.GUID 8fe67e66-6e50-4cfb-9013-da442f327bcf
+
+.AUTHOR user@gmail.com
+
+.COMPANYNAME community
+
+.COPYRIGHT 
+
+.TAGS 
+
+.LICENSEURI 
+
+.PROJECTURI 
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+My first scritp
+
+untested...
+
+
+...good luck :-)
+
+
+
+#> 
+
+
+
+<# 
+
+.DESCRIPTION 
+ My first Script - of cources "Hello World" 
+
+#> 
+Param()
+
+Function Hello-World { Write-Host "Hello World" }
+
